bee004dcbc2767cf5814c13a298ad8e733344eae
Roy Saavedra <rssp@MacBook-Air-de-Roy.local>
2025-12-04 23:57:42 -0500

    fix: update PayU return URL to use dynamic base URL for GitHub Pages

--- Frontend/src/config/payu.config.ts
+++ Frontend/src/config/payu.config.ts
@@ -1,10 +1,13 @@
+const IS_PROD = import.meta.env.PROD;
+const BASE_URL = IS_PROD ? 'https://seb-saav.github.io/ProyectoFinalPW' : 'http://localhost:5173';
+
 export const PAYU_CONFIG = {
   merchantId: '508029',
   accountId: '512321',
   apiKey: '4Vj8eK4rloUd272L48hsrarnUA',
   test: true, 
   paymentUrl: 'https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/',
-  // CORRECCIÓN: Quitamos "/tienda" para que coincida con tu Router
-  responseUrl: 'http://localhost:5173/recarga-exitosa', 
-  confirmationUrl: 'http://localhost:5173/recarga-exitosa',
+  // Configuración dinámica para Local vs GitHub Pages
+  responseUrl: `${BASE_URL}/recarga-exitosa`, 
+  confirmationUrl: `${BASE_URL}/recarga-exitosa`,
 };
